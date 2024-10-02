d597603352547677608c2034f08b7e7ba5499f1e
Humira40mg <nathanmillot@icloud.com>
2024-10-02 11:03:05 +1100

    acceuil html, css and js.
    Initialisation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Site_Static
  
+This is a simple static website for showcasing a lodge in New Caledonia. The site includes basic information about the lodge, its amenities, and contact details. Work in progress.
---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', (evt) => {
+        menu.classList.toggle('active');
+    });
+});
+
+
+
